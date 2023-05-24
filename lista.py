182b4c76ab90ee1f3d40c048f19860695998e0f8
edilson gomes de lima <edskateicevida@gmail.com>
2023-05-24 03:02:52 -0300

    lista de nomes

---
+++ lista.py
@@ -0,0 +1,5 @@
+lista = [1, 2, 3, 4, 5]
+
+
+print(lista[0])  # Saída: 1
+print(lista[2])  # Saída: 3
---
+++ lista01.py
@@ -0,0 +1,6 @@
+nomes = ["João", "Maria", "Pedro", "Ana", "Carlos"]
+
+# Acessando elementos da lista
+print(nomes[0])  # Saída: João
+print(nomes[2])  # Saída: Pedro
+print(nomes[4]) # saida: carlos
