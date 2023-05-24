750c655b3c2a113580dad065db32011a1d9e7df0
edilson gomes de lima <edskateicevida@gmail.com>
2023-05-24 02:26:44 -0300

    Programa python

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-2 + 2 
+nome = input("Qual é o seu nome? ")
+
+print("Olá, " + nome + "! Bem-vindo ao programa Python.")
+
+# Calculadora de soma
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print("A soma de", num1, "+", num2, "é igual a", soma)
